a263f9ac4ce6e48a2a167d9bac9dd718120c0fec
sh1ma <in9lude@gmail.com>
2020-08-23 13:49:23 +0900

    raise for status

--- src/request.py
+++ src/request.py
@@ -13,24 +13,29 @@ class HttpClient(httpx.Client):
     def get_request(self, path: Path, params: typing.Dict):
         params = pick_params(path, params)
         res = self.get(path, params=params)
+        res.raise_for_status()
         return res
 
     def post_request(self, path: Path, params: typing.Dict):
         params = pick_params(path, params)
         res = self.post(path, params=params)
+        res.raise_for_status()
         return res
 
     def put_request(self, path: Path, params: typing.Dict):
         params = pick_params(path, params)
         res = self.put(path, params=params)
+        res.raise_for_status()
         return res
 
     def delete_request(self, path: Path, params: typing.Dict):
         params = pick_params(path, params)
         res = self.delete(path, params=params)
+        res.raise_for_status()
         return res
 
     def patch_request(self, path: Path, params: typing.Dict):
         params = pick_params(path, params)
         res = self.patch(path, params=params)
+        res.raise_for_status()
         return res
