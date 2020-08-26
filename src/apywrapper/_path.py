8c7eec2dbaa4fba5ddd10450c6dee849da4f924f
sh1ma <in9lude@gmail.com>
2020-08-27 03:22:19 +0900

    型つけた

--- src/apywrapper/_path.py
+++ src/apywrapper/_path.py
@@ -1,6 +1,5 @@
-from ._types import ParamsType
 import string
-
+from typing import Dict
 
 formatter = string.Formatter()
 
@@ -10,11 +9,11 @@ class Path(str):
     Path
     """
 
-    def __new__(cls, path: str, params: ParamsType) -> str:
+    def __new__(cls, path: str, params: Dict) -> str:
         formatted = path.format(**params)
         path = super().__new__(cls, formatted)  # type: ignore
         return path
 
-    def __init__(self, path: str, params: ParamsType):
+    def __init__(self, path: str, params: Dict):
         # pylint: disable=super-init-not-called, unused-argument
         self.required_args = [e[1] for e in formatter.parse(path)]
