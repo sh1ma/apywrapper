aef543d89ecbb648bc73ef531e3dd10f54a1bbfc
sh1ma <in9lude@gmail.com>
2020-09-01 18:41:11 +0900

    #1

--- src/apywrapper/__init__.py
+++ src/apywrapper/__init__.py
@@ -1,3 +1,3 @@
-from ._api import Apy
+from ._api import Apy, get, post, put, delete, patch
 
-__all__ = ["Apy"]
+__all__ = ["Apy", "get", "post", "put", "delete", "patch"]
--- src/apywrapper/_api.py
+++ src/apywrapper/_api.py
@@ -4,6 +4,58 @@ from ._abc import Api
 from ._request import HttpClient, make_request
 
 
+def get(path):
+    def _get(func):
+        def wrapper(self, *args):
+            return make_request(path, self.http_client.get_request)(func)(self, *args)
+
+        return wrapper
+
+    return _get
+
+
+def post(path):
+    def _post(func):
+        def wrapper(self, *args):
+            return make_request(path, self.http_client.post_request)(func)(self, *args)
+
+        return wrapper
+
+    return _post
+
+
+def put(path):
+    def _put(func):
+        def wrapper(self, *args):
+            return make_request(path, self.http_client.put_request)(func)(self, *args)
+
+        return wrapper
+
+    return _put
+
+
+def delete(path):
+    def _delete(func):
+        def wrapper(self, *args):
+            return make_request(path, self.http_client.delete_request)(func)(
+                self, *args
+            )
+
+        return wrapper
+
+    return _delete
+
+
+def patch(path):
+    def _patch(func):
+        def wrapper(self, *args):
+            return make_request(path, self.http_client.patch_request)(func)(self, *args)
+
+        return wrapper
+
+    return _patch
+
+
 class Apy(Api):
     """
     Apy class
@@ -12,17 +64,34 @@ class Apy(Api):
     def __init__(self, host: str, headers: HeaderTypes) -> None:
         self.http_client = HttpClient(base_url=host, headers=headers)
 
-    def get(self, path: str):
-        return make_request(path, self.http_client.get_request)
+    def get(self, path: str, request_func=None):
+        request_func = (
+            request_func if request_func is not None else self.http_client.get_request
+        )
+        return make_request(path, request_func)
 
-    def post(self, path: str):
-        return make_request(path, self.http_client.post_request)
+    def post(self, path: str, request_func=None):
+        request_func = (
+            request_func if request_func is not None else self.http_client.post_request
+        )
+        return make_request(path, request_func)
 
-    def put(self, path: str):
-        return make_request(path, self.http_client.put_request)
+    def put(self, path: str, request_func=None):
+        request_func = (
+            request_func if request_func is not None else self.http_client.put_request
+        )
+        return make_request(path, request_func)
 
-    def delete(self, path: str):
-        return make_request(path, self.http_client.delete_request)
+    def delete(self, path: str, request_func=None):
+        request_func = (
+            request_func
+            if request_func is not None
+            else self.http_client.delete_request
+        )
+        return make_request(path, request_func)
 
-    def patch(self, path: str):
-        return make_request(path, self.http_client.patch_request)
+    def patch(self, path: str, request_func=None):
+        request_func = (
+            request_func if request_func is not None else self.http_client.patch_request
+        )
+        return make_request(path, request_func)
