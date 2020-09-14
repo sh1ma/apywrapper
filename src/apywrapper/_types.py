6138383920273d87a19acf95f65c9ab446d9602e
sh1ma <in9lude@gmail.com>
2020-09-14 17:57:27 +0900

    support pydantic

--- src/apywrapper/_types.py
+++ src/apywrapper/_types.py
@@ -8,13 +8,24 @@ from typing import Callable, Dict, List, Optional, Protocol, Union
 from httpx import Response
 
 
-class EntityType(Protocol):
+class DataclassEntityType(Protocol):
     # pylint: disable=too-few-public-methods
     """
-    Entity Object Type
+    Entity Object Type on dataclass
     """
 
     __dataclass_fields__: Dict
+
+
+class PydanticEntityType(Protocol):
+    """
+    Entity Object Type on Pydantic
+    """
+
+    __fields__: Dict
+
+
+EntityType = Union[DataclassEntityType, PydanticEntityType]
 
 
 Entity = Optional[Union[List[EntityType], EntityType]]
